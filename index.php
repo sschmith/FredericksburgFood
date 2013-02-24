91b761f7f2d0ff73b03b7f3f49f56bef913206cd
Steve Schmith <sschmith@mail.umw.edu>
2013-02-24 18:52:39 -0500

    Updated home page

--- index.php
+++ index.php
@@ -74,7 +74,12 @@ fred<span>food</span>
   <img src="images/img1.jpg" width="204" height="128" alt="img1"></div>
   <div class="fttxt">
   <h3>Log In</h3>
-  <p> login stuff here </p>
+  <form action="submitLogin.php" method="post">
+  <table>
+  <tr><td>Username</td><td><input type=text name=login id=login size=20/></td></tr>
+  <tr><td>Password</td><td><input type=password name=password id=password size=20/></td></tr>
+  <tr><td><input type="submit" value="Log In"/ style="height: 25px; width: 100px" ></tr></td>
+  </table>
   </div>
   </div>
 </section>
@@ -85,7 +90,8 @@ News
 </h2>
 <p> We could add news updates here at some point, i.e. "user x gave 5 stars to burger king," or it could be used by admins (easier) </p>
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'blog_posts');
+/*
+$db = mysqli_connect('localhost', 'root', '', '');
 $query = "SELECT * FROM blogs ORDER BY Date";
 $result = mysqli_query($db, $query);
 while($row = mysqli_fetch_array($result))
@@ -99,6 +105,7 @@ echo "<header><h3>$author</h3><p class=\"postinfo\">Published on <time>$currentd
 echo "<p>$blog</p><footer><span class=\"author\">$author</span><span class=\"permalink\"><a href=\"#permalink\">Read Full</a></span>";
 echo "<span class=\"comments\"><a href=\"#\">0 Comments</a></span></footer></article>";
 }
+*/
 ?>
 <div class="wp-pagenavi">
 <span class="current">1</span><a href="/page/2/" title="2">2</a><a href="/page/3/" title="3">3</a><a href="/page/4/" title="4">4</a><a href="/page/5/" title="5">5</a><a href="/page/6/" title="6">6</a><a href="/page/7/" title="7">7</a><a href="/page/8/" title="8">8</a><a href="/page/2/">Next &raquo;</a><span class="extend">...</span><a href="/page/27/" title="Last &raquo;">Last &raquo;</a></div>
@@ -107,20 +114,14 @@ echo "<span class=\"comments\"><a href=\"#\">0 Comments</a></span></footer></art
 </section>
 <section id="sidebar">
 <div id="sidebarwrap">
-<h2>About SilverBlog</h2>
-<p>SilverBlog is a free CSS Template released under GNU GPL license. You are free to use / modify it in any way you want without any restrictions.  A link back to this website will always be appreciated. <a href="#">Read More</a></p>
+<h2>About FredFood</h2>
+<p> FredFood gives you an avenue to review your favorite restaurants in the Fredericksburg, VA area. Share it with your friends!</p>
 
 <h2>Categories</h2>
 <ul>
 
-	<li><a href="#">Web Design</a>(4)</li>
-	<li><a href="#">Graphics Design</a>(8)</li>
-	<li><a href="#">Computers</a>(12)</li>
-	<li><a href="#">Typography</a>(3)</li>
-	<li><a href="#">Photogrphy</a>(4)</li>
-	<li><a href="#">Mathematics</a>(5)</li>
-	<li><a href="#">General News</a>(24)</li>
-	<li><a href="#">Music and Entertainment</a>(1)</li>
+	<li><a href="#">Restaurant Reviews</a></li>
+	<li><a href="#">User Profiles</a></li>
 
 
 </ul>
@@ -128,14 +129,7 @@ echo "<span class=\"comments\"><a href=\"#\">0 Comments</a></span></footer></art
 
 <ul>
 
-	<li><a href="#">Web Design</a></li>
-	<li><a href="#">Graphics Design</a></li>
-	<li><a href="#">Computers</a></li>
-	<li><a href="#">Typography</a></li>
-	<li><a href="#">Photogrphy</a></li>
-	<li><a href="#">Mathematics</a></li>
-	<li><a href="#">General News</a></li>
-	<li><a href="#">Music and Entertainment</a></li>
+	<li><a href="#">Set this to auto-update with new stories</a></li>
 
 
 </ul>

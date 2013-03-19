5f934bb0e5fe8c80bab5c695673c7b8e15dd9a81
Steve Schmith <sschmith@umw.edu>
2013-03-19 00:03:03 -0400

    Search now produces links to restaurant reviews.

--- restaurantReview.php
+++ restaurantReview.php
@@ -4,62 +4,44 @@
 <?php
  			require("includes/header.php");
 ?>
-<div id="leftcontainer">
-<h2 class="mainheading">
-Search Results
 
-</h2>
-<script language="JavaScript" type="text/javascript">
-<!--
-function getRestaurant ( selectedtype )
-{
-  document.restaurantform.restName.value = selectedtype ;
-  document.restaurantform.submit() ;
-}
--->
-</script>
-<?php
+<?php	
 	$db = mysqli_connect('localhost', 'root', '', 'restaurant_reviews');
 
-	$searchTerms = $_POST['search'];
-	$searchClass = $_POST['searchClass'];
-	$query = '';
+	$restName = $_POST['restName'];
 	
-	if($searchClass == 'Name')
-	{
-		$query = "SELECT * FROM restaurants WHERE Name='$searchTerms'";
-	}
-	else if($searchClass == 'Area')
-	{
-		$query = "SELECT * FROM restaurants WHERE Area='$searchTerms'";
-	}
-	else
-	{
-		$query = "SELECT * FROM restaurants WHERE Avg_Rating >= $searchTerms";
-	}
+	echo "<div id=\"leftcontainer\"><h2 class=\"mainheading\">Reviews for <b>$restName</b></h2>";
 	
-	$result = mysqli_query($db,$query);
-	
-	echo "<form name=\"restaurantform\" action=\"restaurantReview.php\" method=\"post\">";
-	echo "<input type=\"hidden\" name=\"restName\">";
+	$query = "SELECT * FROM reviews WHERE Restaurant_Name='$restName'";
+	$result = mysqli_query($db, $query);
 	
 	while($row = mysqli_fetch_array($result))
 	{
-		// Pull data from results
-		$name = $row['Name'];
-		$streetNum = $row['Street_Number'];
-		$streetName = $row['Street_Name'];
-		$area = $row['Area'];
-		$phone = $row['Phone'];
+		$author = $row['Reviewed_By'];
+		$numStars = $row['Num_Stars'];
+		$priceRating = $row['Price_Rating'];
+		$review = $row['Review_Text'];
 		
+		echo "<hr><table>";
+		echo "<tr><td><b>Author</b>:</td><td>$author</td></tr>";
+		echo "<tr><td><b>Quality</b>:</td><td>";
 		
-		echo "<p>_________________________________________________________<br/>";
-		echo "<b><a href=\"javascript:getRestaurant('$name')\">$name</a></b><br/>";
-		echo "$streetNum $streetName, $area, VA <br/>";
-		echo "Phone: $phone<br/>";
-		echo "_________________________________________________________</p>";
+		for($i = 0; $i < $numStars; $i++)
+		{
+			echo "★";
+		}
+		echo "</td></tr>";
+		echo "<tr><td><b>Value</b>: </td><td>";
+		
+		for($i = 0; $i < $numStars; $i++)
+		{
+			echo "★";
+		}
+		echo "</td></tr>";
+		echo "<tr><td><b>Review</b>: </td>";
+		echo "<td>$review</td></tr></table>";
 	}
-	echo "</form>";
+	echo "<hr>";
 ?>
 <div class="wp-pagenavi">
 <span class="current">1</span><a href="/page/2/" title="2">2</a><a href="/page/3/" title="3">3</a><a href="/page/4/" title="4">4</a><a href="/page/5/" title="5">5</a><a href="/page/6/" title="6">6</a><a href="/page/7/" title="7">7</a><a href="/page/8/" title="8">8</a><a href="/page/2/">Next &raquo;</a><span class="extend">...</span><a href="/page/27/" title="Last &raquo;">Last &raquo;</a></div>

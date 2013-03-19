ec98aaa40478ee6a340e63d256b7320b910a4652
Steve Schmith <sschmith@umw.edu>
2013-03-19 00:35:12 -0400

    Updated sql file

--- restaurant_reviews.sql
+++ restaurant_reviews.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2013 at 05:57 PM
+-- Generation Time: Mar 19, 2013 at 05:33 AM
 -- Server version: 5.5.27
 -- PHP Version: 5.4.7
 
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `restaurants` (
   `Street_Number` int(11) DEFAULT NULL,
   `Street_Name` varchar(40) NOT NULL,
   `Area` varchar(40) NOT NULL,
-  `Phone` varchar(10) DEFAULT NULL,
+  `Phone` int(10) DEFAULT NULL,
   `Has_Bar` char(1) NOT NULL DEFAULT 'U',
   `Open_Past_10` char(1) NOT NULL DEFAULT 'U',
   `Accepts_EagleOne` char(1) NOT NULL DEFAULT 'U',
@@ -44,11 +44,13 @@ CREATE TABLE IF NOT EXISTS `restaurants` (
 --
 
 INSERT INTO `restaurants` (`Name`, `Street_Number`, `Street_Name`, `Area`, `Phone`, `Has_Bar`, `Open_Past_10`, `Accepts_EagleOne`, `Price_Rating`, `Avg_Rating`) VALUES
-('Burger King', 1190, 'Burger King Drive', 'Fredericksburg', '5401234567', 'F', 'T', 'T', 5, 3),
-('Hunan Garden', 9876, 'Walker Ln', 'Fredericksburg', '5407653322', 'F', 'F', 'F', 3, 4),
-('Roadkill Roadhouse', 1111, 'Old Country Road', 'Dahlgren', '5407774444', 'T', 'T', 'F', 5, 1),
-('Good Food Place', 4444, 'Blah St', 'Fredericksburg', '5402223333', 'U', 'U', 'U', 1, 1),
-('', 0, '', '', '', 'U', 'U', 'U', 1, 1);
+('Seafood Place', 22222, 'Waffle Way', 'Dahlgren', 123, 'U', 'U', 'U', 1, 1),
+('', 0, '', '', 0, 'U', 'U', 'U', 1, 1),
+('test', 0, '1113', 'test', 0, 'U', 'U', 'U', 1, 1),
+('Testing Restaurant', 65820, 'H Street', 'Fredericksburg', 555, 'U', 'U', 'U', 1, 1),
+('Chipotle', 67800, 'Long Street', 'Stafford', 2147483647, 'U', 'U', 'U', 1, 1),
+('Red Lobster', 67111, 'River Road', 'Fredericksburg', 2147483647, 'U', 'U', 'U', 1, 1),
+('Baskin & Robbins', 7462, 'Ice Cream Way', 'Dahlgren', 2147483647, 'U', 'U', 'U', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -69,8 +71,17 @@ CREATE TABLE IF NOT EXISTS `reviews` (
 --
 
 INSERT INTO `reviews` (`Restaurant_Name`, `Reviewed_By`, `Num_Stars`, `Price_Rating`, `Review_Text`) VALUES
-('Good Food Place', 'Steve', 1, 1, 0x476f6f6420706c6163652e),
-('', '', 1, 1, '');
+('Burger King', 'admin', 1, 1, ''),
+('Seafood Place', 'admin', 1, 1, 0x49742077617320616d617a696e6720776f7720736f20676f6f64),
+('', 'admin', 1, 1, ''),
+('test', 'admin', 1, 1, 0x74657374),
+('Testing Restaurant', 'admin', 1, 1, 0x616c73646b6a66616c736a64666c3b6173),
+('Chipotle', 'admin', 1, 1, 0x616b7366646a613b646b6a6661646a666173646b6a663b61646b6664613b646a663b616a64613b),
+('Red Lobster', 'admin', 1, 1, 0x476f6f6420736561666f6f64),
+('Red Lobster', 'admin', 4, 3, 0x457870656e736976652062757420666f6f6420697320676f6f64),
+('Baskin & Robbins', 'admin', 1, 1, 0x49206c696b652069636520637265616d),
+('Red Lobster', 'admin', 3, 4, 0x49206c696b652069636520637265616d20746f6f),
+('Baskin & Robbins', 'admin', 4, 3, 0x74657374);
 
 -- --------------------------------------------------------
 
@@ -79,17 +90,19 @@ INSERT INTO `reviews` (`Restaurant_Name`, `Reviewed_By`, `Num_Stars`, `Price_Rat
 --
 
 CREATE TABLE IF NOT EXISTS `user_info` (
-  `username` varchar(15) NOT NULL DEFAULT '',
-  `password` varchar(12) NOT NULL DEFAULT '',
-  `real_name` varchar(25) DEFAULT NULL
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `isAdmin` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_info`
 --
 
-INSERT INTO `user_info` (`username`, `password`, `real_name`) VALUES
-('admin', 'password', 'FredFood Team');
+INSERT INTO `user_info` (`username`, `password`, `isAdmin`) VALUES
+('admin', 'password', 1),
+('bob', 'marley', 0),
+('newAccount', 'goodpassword', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
